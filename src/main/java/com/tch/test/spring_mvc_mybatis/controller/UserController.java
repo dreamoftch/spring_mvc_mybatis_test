f3d9dfb95b2c5475c7db3874982262c4b9c71660
tianchaohui <tianchaohui.sh@superjia.com>
2016-03-31 14:59:54 +0800

    use velocity

--- src/main/java/com/tch/test/spring_mvc_mybatis/controller/UserController.java
+++ src/main/java/com/tch/test/spring_mvc_mybatis/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,16 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.tch.test.spring_mvc_mybatis.model.User;
 import com.tch.test.spring_mvc_mybatis.service.user.UserService;
 
+@Controller
 @RequestMapping("/userController")
 public class UserController {
-	
+	 
 	@Autowired
 	private UserService userService;
 
 	@RequestMapping("/index")
 	public String index(HttpServletRequest request, Model model){
-		model.addAttribute("user", userService.getAllUsers());
-		return "velocity/index.vm";
+		model.addAttribute("users", userService.getAllUsers());
+		return "user/index";
 	}
 	
 	@RequestMapping("/getAllUsers")
--- src/main/java/com/tch/test/spring_mvc_mybatis/serviceimpl/user/UserServiceImpl.java
+++ src/main/java/com/tch/test/spring_mvc_mybatis/serviceimpl/user/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.tch.test.spring_mvc_mybatis.serviceimpl.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tch.test.spring_mvc_mybatis.dao.user.UserMapper;
 import com.tch.test.spring_mvc_mybatis.model.User;
 import com.tch.test.spring_mvc_mybatis.service.user.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
